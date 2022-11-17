b230a504a7fbdb6c60f8bb8c6f1cbfbd0ebefcfe
carinette1364 <carineguyonnet@gmail.com>
2022-11-17 14:16:41 +0100

    add page program

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,12 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
+
+
 class ProgramController extends AbstractController
 {
       /**
      * @Route("/program/", name="program_index")
      */
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
+
     public function index(): Response
     {
         // return new Response(
@@ -21,4 +25,13 @@ class ProgramController extends AbstractController
             'website' => 'Wild Series',
          ]);
     }
+
+    #[Route('/{id}', methods:['GET'], requirements: ['id' => '\d+'], name: 'show')]
+    public function show(int $id=4): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+         ]);
+    }
+
 }
